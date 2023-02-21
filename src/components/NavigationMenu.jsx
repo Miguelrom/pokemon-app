f29da0ab3dd50e2436453e40a9d58ef0b160df68
Miguel Romero <Miguelrom@users.noreply.github.com>
2023-02-21 15:30:28 -0700

    Add responsive desktop-size styles and functionality

--- src/components/NavigationMenu.jsx
+++ src/components/NavigationMenu.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+
+import useMediaQuery from '../hooks/useMediaQuery';
 import classes from './NavigationMenu.module.css';
 
 export default function NavigationMenu () {
 
   const [showMenu, setShowMenu] = useState(false);
+  const matchesMedia = useMediaQuery('(min-width: 768px)');
 
   const toggleMenuHandler = () => {
-    setShowMenu(prevState => !prevState);
+    setShowMenu((prevState) => !prevState);
   }
+
 
   return (
     <header className={classes.header}>
@@ -16,16 +20,14 @@ export default function NavigationMenu () {
         <NavLink to="/" className={classes.brandLink}>Pokemon Adopter</NavLink>
         <button className={classes.togglerBtn} onClick={toggleMenuHandler}>Toggle Menu</button>
       </div>
-      {showMenu &&
-        <nav>
-          <ul className={classes.list}>
-            <li><NavLink to="pokemon">Pokemon</NavLink></li>
-            <li><NavLink to="adoptions">Adoptions</NavLink></li>
-            <li><NavLink to="about">About</NavLink></li>
-            <li><NavLink to="checkout">Checkout <span className={classes.badge}>{100}</span> </NavLink></li>
-          </ul>
-        </nav>
-      }
+      <nav className={showMenu || matchesMedia ? classes.menuOn : classes.menuOff}>
+        <ul className={classes.list}>
+          <li><NavLink to="pokemon" className={(isActive) => isActive ? classes.active : undefined}>Pokemon</NavLink></li>
+          <li><NavLink to="adoptions" className={(isActive) => isActive ? classes.active : undefined}>Adoptions</NavLink></li>
+          <li><NavLink to="about" className={(isActive) => isActive ? classes.active : undefined}>About</NavLink></li>
+          <li><NavLink to="checkout" className={(isActive) => isActive ? classes.active : undefined}>Checkout <span className={classes.badge}>{100}</span> </NavLink></li>
+        </ul>
+      </nav>
     </header>
   );
 
