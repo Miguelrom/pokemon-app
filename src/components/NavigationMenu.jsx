0d1de50eaeeb8b75b7bf3f9b32baaff03cc93051
Miguel Romero <Miguelrom@users.noreply.github.com>
2023-02-20 17:19:34 -0700

    Refactor NavigationMenu component to mobile first

--- src/components/NavigationMenu.jsx
+++ src/components/NavigationMenu.jsx
@@ -1,19 +1,31 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import classes from './NavigationMenu.module.css';
 
 export default function NavigationMenu () {
 
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenuHandler = () => {
+    setShowMenu(prevState => !prevState);
+  }
+
   return (
     <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li><NavLink className={classes.menuLink} to="/">Home</NavLink></li>
-          <li><NavLink to="pokemon">Pokemon</NavLink></li>
-          <li><NavLink to="adoptions">Adoptions</NavLink></li>
-          <li><NavLink to="about">About</NavLink></li>
-          <li><NavLink to="checkout">Checkout <span className={classes.badge}>{100}</span> </NavLink></li>
-        </ul>
-      </nav>
+      <div className={classes.home}>
+        <NavLink to="/" className={classes.brandLink}>Pokemon Adopter</NavLink>
+        <button className={classes.togglerBtn} onClick={toggleMenuHandler}>Toggle Menu</button>
+      </div>
+      {showMenu &&
+        <nav>
+          <ul className={classes.list}>
+            <li><NavLink to="pokemon">Pokemon</NavLink></li>
+            <li><NavLink to="adoptions">Adoptions</NavLink></li>
+            <li><NavLink to="about">About</NavLink></li>
+            <li><NavLink to="checkout">Checkout <span className={classes.badge}>{100}</span> </NavLink></li>
+          </ul>
+        </nav>
+      }
     </header>
   );
 
