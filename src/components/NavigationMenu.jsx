aca182f79d9cb33fce122218c123b641f8f0dfb7
Miguel Romero <Miguelrom@users.noreply.github.com>
2023-02-22 12:16:24 -0700

    Fix nav links active function

--- src/components/NavigationMenu.jsx
+++ src/components/NavigationMenu.jsx
@@ -22,10 +22,10 @@ export default function NavigationMenu () {
       </div>
       <nav className={showMenu || matchesMedia ? classes.menuOn : classes.menuOff}>
         <ul className={classes.list}>
-          <li><NavLink to="pokemon" className={(isActive) => isActive ? classes.active : undefined}>Pokemon</NavLink></li>
-          <li><NavLink to="adoptions" className={(isActive) => isActive ? classes.active : undefined}>Adoptions</NavLink></li>
-          <li><NavLink to="about" className={(isActive) => isActive ? classes.active : undefined}>About</NavLink></li>
-          <li><NavLink to="checkout" className={(isActive) => isActive ? classes.active : undefined}>Checkout <span className={classes.badge}>{100}</span> </NavLink></li>
+          <li><NavLink to="pokemon" className={({isActive}) => isActive ? classes.active : undefined}>Pokemon</NavLink></li>
+          <li><NavLink to="adoptions" className={({isActive}) => isActive ? classes.active : undefined}>Adoptions</NavLink></li>
+          <li><NavLink to="about" className={({isActive}) => isActive ? classes.active : undefined}>About</NavLink></li>
+          <li><NavLink to="checkout" className={({isActive}) => isActive ? classes.active : undefined}>Checkout <span className={classes.badge}>{100}</span> </NavLink></li>
         </ul>
       </nav>
     </header>
