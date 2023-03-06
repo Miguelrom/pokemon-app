f3ed72d7668d3a2f1deb08677d9be6924f05c637
Miguel Romero <Miguelrom@users.noreply.github.com>
2023-03-06 12:07:33 -0700

    Add scroll restoration
    Add the ScrollRestoration component in order to enable scrolling to the
    top of the page when changing pages.

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import NavigationMenu from "../components/NavigationMenu";
 
 export default function Root () {
@@ -8,6 +8,7 @@ export default function Root () {
       <main>
         <Outlet />
       </main>
+      <ScrollRestoration />
     </>
   );
 }
