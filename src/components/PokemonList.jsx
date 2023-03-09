bab253ec48ce6fa77cef9c671de467e1408e4229
Miguel Romero <Miguelrom@users.noreply.github.com>
2023-03-08 17:26:12 -0700

    Change flexbox layout for grid in list.
    The three rows with flexbox layout were changed for a grid layout to
    handle a variable number of items and to get better alignment.

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -25,33 +25,20 @@ import classes from './PokemonList.module.css';
 
 export default function PokemonList ({pokemon, onPageChange, page}) {
 
+  const list = pokemon.map((item) => <PokemonCard {...item}/>);
+
   return (
-    <div>
-      <div className={classes.row}>
-        <PokemonCard {...pokemon[0]} isAdopted={false}/>
-        <PokemonCard {...pokemon[1]} isAdopted={false}/>
-        <PokemonCard {...pokemon[2]} isAdopted={false}/>
-      </div>
-      <div className={classes.row}>
-        <PokemonCard {...pokemon[3]} isAdopted={false}/>
-        <PokemonCard {...pokemon[4]} isAdopted={false}/>
-        <PokemonCard {...pokemon[5]} isAdopted={false}/>
-      </div>
-      <div className={classes.row}>
-        <PokemonCard {...pokemon[6]} isAdopted={false}/>
-        <PokemonCard {...pokemon[7]} isAdopted={false}/>
-        <PokemonCard {...pokemon[8]} isAdopted={false}/>
-      </div>
-      <div className={classes.row}>
-        <PokemonCard {...pokemon[9]} isAdopted={false}/>
-        <PokemonCard {...pokemon[10]} isAdopted={false}/>
-        <PokemonCard {...pokemon[11]} isAdopted={false}/>
+    <>
+      <div className={classes.listContainer}>
+        <div className={classes.pokeGrid}>
+          {list}
+        </div>
       </div>
       <div className={classes.pageControl}>
         <button onClick={() => onPageChange('previous')} disabled={page===1}>Previous</button>
         <span> <span style={{fontWeight: "800"}}>{page}</span> of 84</span>
         <button onClick={() => onPageChange('next')} disabled={page===84}>Next</button>
       </div>
-    </div>
+    </>
   );
 }
