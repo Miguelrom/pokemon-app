1bab24b21b846ea6b66cbdb5e7a13de40f47d9eb
Miguel Romero <Miguelrom@users.noreply.github.com>
2023-03-27 15:59:29 -0700

    Add list of adopted pokemon.

--- src/pages/Adoptions.jsx
+++ src/pages/Adoptions.jsx
@@ -1,7 +1,18 @@
+import { useSelector } from "react-redux";
+import { selectAdopted } from "../store/userSlice";
+import PokemonList from "../components/PokemonList";
+import PageTitle from "../components/ui/PageTitle";
+
 
 export default function Adoptions () {
 
+  const adopted = useSelector(selectAdopted);
+
   return (
-    <h1>Adoptions page...</h1>
+    <>
+      <PageTitle>Your companions!</PageTitle>
+      {adopted.length > 0 && <PokemonList pokemon={adopted}/>}
+      {adopted.length === 0 && <p style={{textAlign: 'center'}}>No adopted pokemon yet</p>}
+    </>
   );
 }
