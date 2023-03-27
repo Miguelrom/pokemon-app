988e791e0d50335d6eb2f336fb70a3ff6705e13b
Miguel Romero <Miguelrom@users.noreply.github.com>
2023-03-27 15:54:55 -0700

    Add new title and Cart component

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,14 @@
+import Cart from "../components/Cart";
+import PageTitle from "../components/ui/PageTitle";
 
-export default function Checkout () {
+
+export default function Checkout() {
 
   return (
-    <h1>Checkout page...</h1>
+    <>
+      <PageTitle>Your new friends!</PageTitle>
+      <Cart />
+    </>
   );
+
 }
