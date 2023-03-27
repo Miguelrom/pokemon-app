5368c3b7f06a489e63e5a8de9a0b88c1d02ec2cb
Miguel Romero <Miguelrom@users.noreply.github.com>
2023-03-27 16:00:57 -0700

    Remove dummy data.

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -2,27 +2,6 @@ import PokemonCard from "./PokemonCard";
 import classes from './PokemonList.module.css';
 
 
-// const POKEMON_PREVIEW_LIST = [
-//   {
-//     id: 1,
-//     name: "bulbasaur",
-//     sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-//     types: ["grass", "poison"],
-//   },
-//   {
-//     id: 2,
-//     name: "ivysaur",
-//     sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
-//     types: ["grass", "poison"],
-//   },
-//   {
-//     id: 3,
-//     name: "venusaur",
-//     sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
-//     types: ["grass", "poison"],
-//   },
-// ];
-
 export default function PokemonList ({pokemon}) {
 
   const list = pokemon.map((item) => <PokemonCard key={item.id} {...item}/>);
