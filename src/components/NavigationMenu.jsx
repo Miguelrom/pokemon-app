795091db3589869c0bf314b36d502bd77f93182c
Miguel Romero <Miguelrom@users.noreply.github.com>
2023-03-27 20:30:50 -0700

    Add cart length state display

--- src/components/NavigationMenu.jsx
+++ src/components/NavigationMenu.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 import useMediaQuery from '../hooks/useMediaQuery';
 import classes from './NavigationMenu.module.css';
 
@@ -8,6 +8,7 @@ export default function NavigationMenu () {
 
   const [showMenu, setShowMenu] = useState(false);
   const matchesMedia = useMediaQuery('(min-width: 768px)');
+  const cartLength = useSelector(state => state.user.cart.length);
 
   const toggleMenuHandler = () => {
     setShowMenu((prevState) => !prevState);
@@ -25,7 +26,7 @@ export default function NavigationMenu () {
           <li><NavLink to="pokemon" className={({isActive}) => isActive ? classes.active : undefined}>Pokemon</NavLink></li>
           <li><NavLink to="adoptions" className={({isActive}) => isActive ? classes.active : undefined}>Adoptions</NavLink></li>
           <li><NavLink to="about" className={({isActive}) => isActive ? classes.active : undefined}>About</NavLink></li>
-          <li><NavLink to="checkout" className={({isActive}) => isActive ? classes.active : undefined}>Checkout <span className={classes.badge}>{100}</span> </NavLink></li>
+          <li><NavLink to="checkout" className={({isActive}) => isActive ? classes.active : undefined}>Checkout <span className={classes.badge}>{cartLength}</span> </NavLink></li>
         </ul>
       </nav>
     </header>
